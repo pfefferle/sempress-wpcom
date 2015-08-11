619b6e71a311b915c3ae5e90bdea692718e4512c
frankklein <frankklein@d957f892-c61d-0410-b221-f235e6eecf30>
2015-08-11 07:09:28 +0000

    SemPress: Add Responsive Videos support. See #2565.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -17,3 +17,11 @@ function sempress_infinite_scroll_setup() {
 	) );
 }
 add_action( 'after_setup_theme', 'sempress_infinite_scroll_setup' );
+
+/**
+ * Add theme support for Responsive Videos.
+ */
+function sempress_responsive_videos_setup() {
+	add_theme_support( 'jetpack-responsive-videos' );
+}
+add_action( 'after_setup_theme', 'sempress_responsive_videos_setup' );
