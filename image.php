4867495267044cc7ba1efce61e4ce8028ba34b31
sixhours <sixhours@d957f892-c61d-0410-b221-f235e6eecf30>
2017-04-20 20:08:49 +0000

    Sempress : Check for post parent before outputting next, previous, and image attachment information to prevent fatals. See #3643

--- image.php
+++ image.php
@@ -21,24 +21,36 @@ get_header();
 						<div class="entry-meta">
 							<?php
 								$metadata = wp_get_attachment_metadata();
-								printf( __( 'Published <span class="entry-date"><time class="entry-date" datetime="%1$s">%2$s</time></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s" rel="gallery">%8$s</a>', 'sempress' ),
-									esc_attr( get_the_date( 'c' ) ),
-									esc_html( get_the_date() ),
-									wp_get_attachment_url(),
-									$metadata['width'],
-									$metadata['height'],
-									get_permalink( $post->post_parent ),
-									esc_attr( get_the_title( $post->post_parent ) ),
-									get_the_title( $post->post_parent )
-								);
+								if ( $post->post_parent ) {
+									printf( __( 'Published <span class="entry-date"><time class="entry-date" datetime="%1$s">%2$s</time></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s" rel="gallery">%8$s</a>', 'sempress' ),
+										esc_attr( get_the_date( 'c' ) ),
+										esc_html( get_the_date() ),
+										wp_get_attachment_url(),
+										$metadata['width'],
+										$metadata['height'],
+										get_permalink( $post->post_parent ),
+										esc_attr( get_the_title( $post->post_parent ) ),
+										get_the_title( $post->post_parent )
+									);
+								} else {
+									printf( __( 'Published <span class="entry-date"><time class="entry-date" datetime="%1$s">%2$s</time></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a>', 'sempress' ),
+										esc_attr( get_the_date( 'c' ) ),
+										esc_html( get_the_date() ),
+										wp_get_attachment_url(),
+										$metadata['width'],
+										$metadata['height']
+									);
+								}
 							?>
 							<?php edit_post_link( __( 'Edit', 'sempress' ), '<span class="sep"> | </span> <span class="edit-link">', '</span>' ); ?>
 						</div><!-- .entry-meta -->
 
+						<?php if ( $post->post_parent ) { ?>
 						<nav id="image-navigation" class="site-navigation">
 							<span class="previous-image"><?php previous_image_link( false, __( '&larr; Previous', 'sempress' ) ); ?></span>
 							<span class="next-image"><?php next_image_link( false, __( 'Next &rarr;', 'sempress' ) ); ?></span>
 						</nav><!-- #image-navigation -->
+						<?php } ?>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
@@ -46,6 +58,8 @@ get_header();
 						<div class="entry-attachment">
 							<div class="attachment">
 								<?php
+								$next_attachment_url = wp_get_attachment_url();
+								if ( $post->post_parent ) {
 									/**
 									 * Grab the IDs of all the image attachments in a gallery so we can get the URL of the next adjacent image in a gallery,
 									 * or the first image (if we're looking at the last image in a gallery), or, in a gallery of one, just the link to that image file
@@ -75,6 +89,7 @@ get_header();
 										// or, if there's only 1 image, get the URL of the image
 										$next_attachment_url = wp_get_attachment_url();
 									}
+								}
 								?>
 
 								<a href="<?php echo esc_url( $next_attachment_url ); ?>" title="<?php echo esc_attr( get_the_title() ); ?>" rel="attachment"><?php
